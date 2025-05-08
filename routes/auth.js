d878778a0431df81ea4107eb997cf653a5c97342
Nierukochan <sirimongkol58@gmail.com>
2025-05-08 15:05:52 +0700

    2nd commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const { register, login, getAllUsers, approveUser } = require('../controller/auth')
+const { authorize } = require('../middleware/authVerify')
 
 router.post('/register', register)
 router.post('/login',login)
 
 router.get('/getAllUsers',getAllUsers)
 
-router.put('/users/:id/approve', approveUser)
+router.put('/users/:id/approve', authorize, approveUser)
 
 module.exports = router
